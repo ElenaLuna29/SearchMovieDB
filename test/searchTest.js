24bd73af90dc567de7e915e3666eb9b9a40773f6
Elena <elenita21@gmail.com>
2020-02-15 16:47:39 +0000

    added test cases TDD

---
+++ test/searchTest.js
@@ -0,0 +1,48 @@
+var assert = require('chai').assert;
+var sinon = require('sinon');
+var search = require('../src/search');
+
+var documentStub;
+
+before(function(){
+  documentStub = sinon.stub(document, 'getElementById');
+});
+
+describe("Search Page", function(){
+  describe('Listener', function(){
+    it('should add listener for enter key press', function(){
+
+    });
+
+    it('should remove listener when page is destroyed', function(){
+
+    });
+  });
+
+  describe('keyword validation', function(){
+    it('should check keyword is not empty string', function(){
+
+    });
+    it('should check keyword at least two letters long', function(){
+
+    });
+  });
+
+  describe('http request', function(){
+    it('should make http request when keyword is valid', function(){
+
+    });
+    it('should handle response', function(){
+
+    });
+  });
+
+  describe('Parsing results', function(){
+    it('should check results are not empty', function(){
+
+    });
+    it('should parse results into title models', function(){
+
+    });
+  });
+});
